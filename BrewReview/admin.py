79be0530a1d3bf3f866a16d156720d07dc932c33
“Mark <2764158m@student.gla.ac.uk>
2024-03-06 15:19:59 +0000

    Updated Models/Database

--- BrewReview/admin.py
+++ BrewReview/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from BrewReview.models import CoffeeShop, Review, UserProfile
+from BrewReview.models import CoffeeShop, Review, SavedReviews, FavouriteShops, Addresses, UserProfile
 
 class CoffeeShopAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'rating', 'price')
@@ -11,4 +11,7 @@ class ReviewAdmin(admin.ModelAdmin):
 
 admin.site.register(CoffeeShop, CoffeeShopAdmin)
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(SavedReviews)
+admin.site.register(FavouriteShops)
+admin.site.register(Addresses)
 admin.site.register(UserProfile)
