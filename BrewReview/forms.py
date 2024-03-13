1c9ba5c8515aaefd1c779c2cd5189085cc73bcc4
“Mark <2764158m@student.gla.ac.uk>
2024-03-13 13:51:28 +0000

    Address form update

--- BrewReview/forms.py
+++ BrewReview/forms.py
@@ -41,26 +41,17 @@ class CoffeeShopForm(forms.ModelForm):
     postcode = forms.CharField(max_length=128, help_text="Please enter the postcode")
     city = forms.CharField(max_length=128, help_text="Please enter the city")
     country = forms.CharField(max_length=128, help_text="Please enter the country")
-    address = (str(address_line_1) + ", " + str(postcode) + ", " +
-               str(city) + ", " + str(country))
-    #result = (gmaps.geocode(address)[0].get("geometry", None).get("location", None))
-    # if result:
-    #     lat = result.get("lat")
-    #     lng = result.get("lng")
-    # else:
-    #     "invalid addresss"
+
     description = forms.CharField(max_length=CoffeeShop.DESC_MAX_LENGTH, help_text="Please enter a shop description.")
-    image_location_folder = "Not sure yet"
+    #image_location_folder = "Not sure yet"
     serves_food = forms.BooleanField(help_text="Please enter if the shop serves food.", required=False)
-    rating = "Not sure yet"
+    #rating = "Not sure yet"
     price = forms.IntegerField(help_text="Please enter how expensive the shop is on a range 1 to 5.", max_value=5)
 
-    # class Meta:
-    #     model = Addresses
-    #     fields = ('address_line_1', 'postcode', 'city', 'country', )
+
     class Meta:
         model = CoffeeShop
-        fields = ('name','description','serves_food','price','address','lat','lng')
+        fields = ('name','description','serves_food','price',)
 
 
 class ReviewForm(forms.ModelForm):
