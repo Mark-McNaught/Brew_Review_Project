ba3f94e17669bdbdf0f3b843dbd66c0784a42ab0
“Mark <2764158m@student.gla.ac.uk>
2024-03-17 18:04:48 +0000

    Updated reg form, need to add shop owner option and remove mandatory email

--- BrewReview/urls.py
+++ BrewReview/urls.py
@@ -17,8 +17,5 @@ urlpatterns = [
 
     path('profile/', views.profile, name='profile'),
     path('account_settings/', views.account_settings, name='account_settings'),
-    
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.user_logout, name='logout'),
+
 ]
